147995f8a31d5dcdb4dffbabcd3a076ce45a9e63
Mathezzz <andersonmmelo23@outlook.com>
2026-02-17 13:28:03 -0300

    add: logs __init__.py para utilizar como biblioteca

--- logs/logger.py
+++ logs/logger.py
@@ -4,9 +4,6 @@ from config import LOG_LEVEL
 def setup_logger():
     logging.basicConfig(
         level=LOG_LEVEL,
-        format="%(asctime)s [%(levelname)s] %(message)s",
-        handlers=[
-            logging.FileHandler("logs/app.log"),
-            logging.StreamHandler()
-        ]
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
     )
+    return logging.getLogger(__name__)
